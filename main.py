b132aa7c696eb69af2e29acf16e7d32d7d3fbe4e
George Baldwin <georgebaldwin@Georges-MacBook-Air.local>
2023-11-18 17:50:19 +0000

    adding functionaly for adding a movie using TheMovie DataBase api

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
 import requests
 
+TMDB_KEY = os.environ.get("TMDB_API_KEY")
+TMDB_URL = "https://api.themoviedb.org/3/search/movie"
+
+print(TMDB_KEY)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -40,6 +45,11 @@ class RateMovieForm(FlaskForm):
     done = SubmitField('Done')
 
 
+class FindMovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
+
 @app.route("/")
 def home():
     all_movies = db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars()
@@ -48,15 +58,15 @@ def home():
 
 @app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
-    form = RateMovieForm()
+    edit_rating_form = RateMovieForm()
     movie_id = request.args.get("id")
     movie = db.get_or_404(Movie, movie_id)
-    if form.validate_on_submit():
-        movie.rating = float(form.rating.data)
-        movie.review = form.review.data
+    if edit_rating_form.validate_on_submit():
+        movie.rating = float(edit_rating_form.rating.data)
+        movie.review = edit_rating_form.review.data
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template("edit.html", movie=movie, form=form)
+    return render_template("edit.html", movie=movie, rating_form=edit_rating_form)
 
 
 @app.route("/delete", methods=["GET", "POST"])
@@ -66,6 +76,17 @@ def delete_movie():
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for('home'))
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    find_movie_form = FindMovieForm()
+    if find_movie_form.validate_on_submit():
+        movie_title = find_movie_form.title.data
+        response = requests.get(url=TMDB_URL, params={"api_key": TMDB_KEY, "query": movie_title})
+        movie_data = response.json()['results']
+        return render_template('select.html', data=movie_data)
+    return render_template("add.html", find_form=find_movie_form)
 
 
 if __name__ == '__main__':
